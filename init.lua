758c6e4ddbd9b92dae77d88cb091c1d3ac33ba75
skrewby <33297928+skrewby@users.noreply.github.com>
2024-05-08 00:12:15 +1000

    Add leap plugin

--- init.lua
+++ init.lua
@@ -617,6 +617,25 @@ require('lazy').setup({
     'tpope/vim-repeat',
   },
 
+  { -- Better file navigation
+    'ggandor/leap.nvim',
+    enabled = true,
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
+      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
+      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
+    },
+    config = function(_, opts)
+      local leap = require 'leap'
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
