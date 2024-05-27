4ab7137523c2d9a3f1203dde11ea0445cd35a2c6
skrewby <33297928+skrewby@users.noreply.github.com>
2024-05-27 10:45:00 +1000

    Add yazi file tree

--- init.lua
+++ init.lua
@@ -612,6 +612,27 @@ require('lazy').setup({
     'tpope/vim-repeat',
   },
 
+  {
+    'mikavilpas/yazi.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    event = 'VeryLazy',
+    keys = {
+      {
+        -- Open in the current working directory
+        '<leader>t',
+        function()
+          require('yazi').yazi(nil, vim.fn.getcwd())
+        end,
+        desc = 'Open the file manager',
+      },
+    },
+    opts = {
+      open_for_directories = false,
+    },
+  },
+
   { -- Better file navigation
     'ggandor/leap.nvim',
     enabled = true,
